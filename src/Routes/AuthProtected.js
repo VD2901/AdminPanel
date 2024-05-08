03b8f61dbdd3235fbf5d11a7692fe8652314f3f4
Kevingohil <119413567+Henil157@users.noreply.github.com>
2024-05-08 11:06:48 +0530

    clear code

--- src/Routes/AuthProtected.js
+++ src/Routes/AuthProtected.js
@@ -2,9 +2,7 @@ import React, { useEffect } from "react";
 import { Navigate, Route } from "react-router-dom";
 import { setAuthorization } from "../helpers/api_helper";
 import { useDispatch } from "react-redux";
-
 import { useProfile } from "../Components/Hooks/UserHooks";
-
 import { logoutUser } from "../slices/auth/login/thunk";
 
 const AuthProtected = (props) => {
--- src/common/languages.js
+++ src/common/languages.js
@@ -9,37 +9,10 @@ import flagarabic from "../assets/images/flags/ae.svg";
 
 
 const languages = {
-  sp: {
-    label: "Española",
-    flag: flagspain,
-  },
-  gr: {
-    label: "Deutsche",
-    flag: flaggermany,
-  },
-  it: {
-    label: "Italiana",
-    flag: flagitaly,
-  },
-  rs: {
-    label: "русский",
-    flag: flagrussia,
-  },
+
   en: {
     label: "English",
     flag: flagus,
-  },
-  cn: {
-    label: "中国人",
-    flag: flagchina,
-  },
-  fr: {
-    label: "français",
-    flag: flagfrench,
-  },
-  ar: {
-    label: "Arabic",
-    flag: flagarabic,
   },
 }
 
